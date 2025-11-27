4ab8e3e606a9a4ae069f1742de17a34e18b57d09
Apetrei-Alexandru <apetrei.alex2004@gmail.com>
2025-11-27 19:45:58 +0200

    Rezolvare sarcina laborator - timp

---
+++ Controllers/TimePredictionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using static Apetrei_Alexandru_Lab4.TimePredictionModel;
+
+namespace Apetrei_Alexandru_Lab4.Controllers
+{
+    public class TimePredictionController : Controller
+    {
+        public IActionResult Time(ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            var modelPath = "TimePredictionModel.mlnet";
+            ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+
+            // Create prediction engine using loaded model
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Predict trip time (in seconds)
+            ModelOutput result = predEngine.Predict(input);
+
+            // Send result to view
+            ViewBag.Time = result.Score;
+
+            return View(input);
+        }
+    }
+}
