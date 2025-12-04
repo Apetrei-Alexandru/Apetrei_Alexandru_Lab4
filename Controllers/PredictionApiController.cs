6d4b9bf2cf18195916a17349a0fac04c5e8f8c52
Apetrei-Alexandru <apetrei.alex2004@gmail.com>
2025-12-04 17:22:23 +0200

    Ex 1 Lab 6

---
+++ Controllers/PredictionApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Apetrei_Alexandru_Lab4.Data;   // <--- important!
+using Apetrei_Alexandru_Lab4.Models;
+
+namespace Apetrei_Alexandru_Lab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictionApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PredictionApiController(AppDbContext context)  // <--- folosim AppDbContext
+        {
+            _context = context;
+        }
+
+        // GET: api/predictionapi
+        [HttpGet]
+        public async Task<IActionResult> GetPredictions()
+        {
+            // Tabela ta este PredictionHistories
+            var predictions = await _context.PredictionHistories.ToListAsync();
+            return Ok(predictions);
+        }
+
+        // DELETE: api/predictionapi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePrediction(int id)
+        {
+            var prediction = await _context.PredictionHistories.FindAsync(id);
+
+            if (prediction == null)
+            {
+                return NotFound(new { Message = "Predictia nu a fost găsită." });
+            }
+
+            _context.PredictionHistories.Remove(prediction);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Predictia a fost ștearsă cu succes." });
+        }
+    }
+}
