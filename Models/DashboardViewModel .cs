e8dc8bd2cb2fe8118561958321da649b6cc82c17
Apetrei-Alexandru <apetrei.alex2004@gmail.com>
2025-12-07 15:28:30 +0200

    Rezolvare Lab 9

---
+++ Models/DashboardViewModel .cs
@@ -0,0 +1,9 @@
+﻿namespace Apetrei_Alexandru_Lab4.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalPredictions { get; set; }
+        public List<PaymentTypeStat> PaymentTypeStats { get; set; } = new();
+        public List<PriceBucketStat> PriceBuckets { get; set; } = new();
+    }
+}
---
+++ Models/PaymentTypeStat.cs
@@ -0,0 +1,9 @@
+﻿namespace Apetrei_Alexandru_Lab4.Models
+{
+    public class PaymentTypeStat
+    {
+        public string PaymentType { get; set; } = string.Empty;
+        public double AveragePrice { get; set; }
+        public int Count { get; set; }
+    }
+}
